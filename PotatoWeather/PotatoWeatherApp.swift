3b97c19deac2e8edb86596df4689a7ea5cfc7b86
Deathstructor <paulchast@gmail.com>
2023-12-15 13:04:01 +0100

    Initial Commit

---
+++ PotatoWeather/PotatoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoWeatherApp.swift
+//  PotatoWeather
+//
+//  Created by Paul Chastain on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct PotatoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
