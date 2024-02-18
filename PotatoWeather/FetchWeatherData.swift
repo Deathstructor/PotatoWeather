59fb6660c5827f9b99f2936f031ac594bbcfa9e9
Deathstructor <paulchast@gmail.com>
2024-02-18 16:21:48 +0100

    Added 24-hour forecast to the local weather view

--- PotatoWeather/FetchWeatherData.swift
+++ PotatoWeather/FetchWeatherData.swift
@@ -16,14 +16,20 @@ class FetchWeatherData: ObservableObject {
     
     func getWeatherData(lon: CLLocationDegrees, lat: CLLocationDegrees) async throws -> WeatherData {
         
-        
-        let endpoint = "https://opendata-download-metanalys.smhi.se/api/category/mesan2g/version/1/geotype/point/lon/\(round(10000 * lon) / 10000)/lat/\(round(10000 * lat) / 10000)/data.json"
+        let endpoint = "https://opendata-download-metanalys.smhi.se/api/category/mesan2g/version/1/geotype/point/lon/17/lat/59/data.json"
 //        let endpoint = "https://opendata-download-metanalys.smhi.se/api/category/mesan2g/version/1/geotype/point/lon/\(round(10000 * lon) / 10000)/lat/\(round(10000 * lat) / 10000)/data.json"
-        guard let url = URL(string: endpoint) else { throw apiError.invalidURL}
+        
+        guard let url = URL(string: endpoint) 
+        else {
+            throw apiError.invalidURL
+        }
         
         let (data, response) = try await URLSession.shared.data(from: url)
         
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw apiError.invalidResponse }
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 
+        else {
+            throw apiError.invalidResponse
+        }
         
         do {
             let decoder = JSONDecoder()
@@ -34,15 +40,46 @@ class FetchWeatherData: ObservableObject {
     }
     
     
+    
+    func getForecastData(lon: CLLocationDegrees, lat: CLLocationDegrees) async throws -> ForecastData {
+        
+        let endpoint = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17/lat/59/data.json"
+//        let endpoint = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/\(round(10000 * lon) / 10000)/lat/\(round(10000 * lon) / 10000)/data.json"
+        
+        guard let url = URL(string: endpoint) 
+        else {
+            throw apiError.invalidURL
+        }
+        
+        let (data, response) =  try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 
+        else {
+            throw apiError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(ForecastData.self, from: data)
+        } catch {
+            throw apiError.invalidData
+        }
+    }
 }
 
 // Get current temperature data from API
+struct ForecastData: Decodable {
+    let referenceTime: String
+    let timeSeries: [TimeSeries]
+}
+
 struct WeatherData: Decodable {
     let referenceTime: String
     let timeSeries: [TimeSeries]
 }
 
 struct TimeSeries: Decodable {
+    let validTime: String
     let parameters: [Parameters]
 }
 
@@ -50,9 +87,6 @@ struct Parameters: Decodable {
     let name: String
     let values: [Double]
 }
-
-// Get forecast for upcoming day from API
-
 
 // Errors
 enum apiError: Error {
--- PotatoWeather/Views/LocalWeatherView.swift
+++ PotatoWeather/Views/LocalWeatherView.swift
@@ -16,43 +16,110 @@ struct LocalWeatherView: View {
     @StateObject var weatherSymbol = GetWeatherSymbol()
     
     @State var weather: WeatherData?
+    @State var forecast: ForecastData?
     
     var body: some View {
         ZStack {
             if let location = manager.location {
-                if let weather = weather {
-                    VStack(alignment: .trailing, spacing: 10) {
-                        HStack(spacing: 100) {
-                            Text("\(String(describing: weather.timeSeries[0].parameters[0].values[0])) °C")
+                VStack(spacing: 100) {
+                    if let weather = weather {
+                        VStack(alignment: .trailing, spacing: 10) {
+                            let i = weather.timeSeries[0].parameters.firstIndex(where: { $0.name == "Wsymb2" })
+                            HStack(spacing: 100) {
+                                Text("\(String(describing: weather.timeSeries[0].parameters[0].values[0])) °C")
+                                    .fontWeight(.heavy)
+                                    .font(.system(size: 40))
+                                    .fontDesign(.rounded)
+                                    .foregroundStyle(Color.white)
+                                
+                                
+                                weatherSymbol.weatherSymbols[Int(weather.timeSeries[0].parameters[i!].values[0]) - 1]
+                                    .font(.system(size: 50))
+                                    .symbolRenderingMode(.multicolor)
+                                
+                            }
+                            Text(weatherSymbol.weatherString[Int(weather.timeSeries[0].parameters[i!].values[0]) - 1])
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                                .fontDesign(.rounded)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        
+                    } else {
+                        ProgressView()
+                            .task {
+                                do {
+                                    weather = try await fetchWeather.getWeatherData(
+                                        lon: location.longitude,
+                                        lat: location.latitude
+                                    )
+                                } catch {
+                                    print("Error getting weather data: ", error)
+                                }
+                            }
+                    }
+                    
+                    if let forecast = forecast {
+                        
+                        
+                        VStack(alignment: .leading) {
+                            Text("Forecast")
                                 .fontWeight(.heavy)
-                                .font(.system(size: 40))
+                                .font(.system(size: 30))
                                 .fontDesign(.rounded)
                                 .foregroundStyle(Color.white)
-                            
-                            weatherSymbol.weatherSymbols[Int(weather.timeSeries[0].parameters[26].values[0]) - 1]
-                                .font(.system(size: 50))
-                                .symbolRenderingMode(.multicolor)
-                            
-                        }
-                        Text(weatherSymbol.weatherString[Int(weather.timeSeries[0].parameters[26].values[0]) - 1])
-                            .font(.system(size: 20))
-                            .fontWeight(.bold)
-                            .fontDesign(.rounded)
-                            .foregroundStyle(.white)
-                    }
-                } else {
-                    ProgressView()
-                        .task {
-                            do {
-                                weather = try await fetchWeather.getWeatherData(
-                                    lon: location.longitude,
-                                    lat: location.latitude
-                                )
-                            } catch {
-                                print("Error getting weather data: ", error)
+                            ScrollView(.vertical, showsIndicators: false) {
+                                ForEach(0..<24) { day in
+                                    HStack(spacing: 100) {
+                                        let i = forecast.timeSeries[day].parameters.firstIndex(where: { $0.name == "t" })
+                                        
+                                        let timeIndex1 = forecast.timeSeries[0].validTime.index(
+                                            forecast.timeSeries[0].validTime.startIndex,
+                                            offsetBy: 11
+                                        )
+                                        let timeIndex2 = forecast.timeSeries[0].validTime.index(
+                                            forecast.timeSeries[0].validTime.startIndex,
+                                            offsetBy: 13
+                                        )
+                                        
+                                        let index = timeIndex1..<timeIndex2
+                                        
+                                        Text(String(describing: forecast.timeSeries[day].validTime[index]))
+                                            .font(.system(size: 25))
+                                            .fontDesign(.rounded)
+                                            .foregroundStyle(Color.white)
+                                        
+                                        Text("\(String(describing: forecast.timeSeries[day].parameters[i!].values[0])) °C")
+                                            .font(.system(size: 25))
+                                            .fontDesign(.rounded)
+                                            .foregroundStyle(Color.white)
+                                        
+                                        weatherSymbol.weatherSymbols[Int(forecast.timeSeries[day].parameters[18].values[0]) - 1]
+                                            .font(.system(size: 25))
+                                            .symbolRenderingMode(.multicolor)
+                                    }
+                                }
                             }
+                            .frame(height: 345)
+                            
                         }
+                        
+                    } else {
+                        ProgressView()
+                            .task {
+                                do {
+                                    forecast = try await fetchWeather.getForecastData(
+                                        lon: location.longitude,
+                                        lat: location.latitude
+                                    )
+                                } catch {
+                                    print("Error getting forecast data: ", error)
+                                }
+                            }
+                    }
                 }
+                
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
